87386cdcd4ed6de1b3bc27bee8faf7c64c7e6332
hackerzhou <webmaster@hackerzhou.me>
2011-11-08 21:42:13 +0800

    Fix bug in iPad and Android's browsers. Some browser call onResize() multiple times and cause web page refreshing.

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -1,5 +1,7 @@
 // variables
 var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
 
 $(function () {
     // setup garden
@@ -26,7 +28,11 @@ $(function () {
 });
 
 $(window).resize(function() {
-	location.replace(location);
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
 });
 
 function getHeartPoint(angle) {
